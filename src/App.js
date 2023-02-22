b8bc6d3a2d8cdd73b9470fe918ac10680b1f7835
Boluwatife Kamaldeen Lawal <bolulawal5@gmail.com>
2023-02-23 00:09:16 +0100

    remove console logs

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import Routes from "./routes";
 import "./App.css";
 
 function App() {
-  console.log("str",process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
-  console.log("dom",process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
-  console.log("key",process.env.REACT_APP_FIREBASE_APIKEY);
-  console.log("app",process.env.REACT_APP_FIREBASE_APPID);
-  console.log("mea",process.env.REACT_APP_FIREBASE_MEASUREMENT_ID);
   return (
     <div className="App">
       <Routes />
